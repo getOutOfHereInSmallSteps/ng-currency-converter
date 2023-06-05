96a9279a5a51f405a27fd5ee3143d67a8ba17be2
Alex Bihas <alex.bigas.2004@gmail.com>
2023-06-05 19:11:33 +0300

    Separated header and footer components

--- ng-currency-converter/src/app/app.component.ts
+++ ng-currency-converter/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+// import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-currency-converter';
