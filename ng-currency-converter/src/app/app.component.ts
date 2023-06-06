7c4f2de710d97322f56f610d0587b6228aa67b58
Alex Bihas <alex.bigas.2004@gmail.com>
2023-06-06 16:16:14 +0300

    Small bug fixes

--- ng-currency-converter/src/app/app.component.ts
+++ ng-currency-converter/src/app/app.component.ts
@@ -10,15 +10,27 @@ interface ExchangeRate {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  exchangeRates: ExchangeRate | undefined;
+
   baseAmount: number = 0;
   targetAmount: number = 0;
   baseCurrency: string = '';
   targetCurrency: string = '';
 
   currencies: string[] = ['UAH', 'USD', 'EUR', 'GBP'];
-  exchangeRates: ExchangeRate | undefined;
+  headerCurrencies = this.currencies.filter((el) => el !== 'UAH');
+
+  baseCurrencies = this.currencies;
+  targetCurrencies = this.currencies;
 
   convert() {
+    this.baseCurrencies = this.currencies.filter(
+      (el) => el !== this.targetCurrency
+    );
+    this.targetCurrencies = this.currencies.filter(
+      (el) => el !== this.baseCurrency
+    );
+
     if (!this.exchangeRates) return;
     const baseRate = this.exchangeRates[this.baseCurrency];
     const targetRate = this.exchangeRates[this.targetCurrency];
@@ -26,6 +38,13 @@ export class AppComponent {
   }
 
   reverseConvert() {
+    this.baseCurrencies = this.currencies.filter(
+      (el) => el !== this.targetCurrency
+    );
+    this.targetCurrencies = this.currencies.filter(
+      (el) => el !== this.baseCurrency
+    );
+
     if (!this.exchangeRates) return;
     const baseRate = this.exchangeRates[this.baseCurrency];
     const targetRate = this.exchangeRates[this.targetCurrency];
